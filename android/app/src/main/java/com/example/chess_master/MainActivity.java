f1df29c921f5dba27868edec69e3ff6ce8e65cd1
Nitin Mahadev B K <vvce25cse0423@vvce.ac.in>
2026-04-18 12:21:50 +0530

    fix: replace Kotlin MainActivity with Java to fix FlutterActivity compile error

---
+++ android/app/src/main/java/com/example/chess_master/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.chess_master;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/chess_master/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chess_master
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
